b1c41ba3ba0256c28e24fbb7bef196edbd801a8e
Mattbox <mattbox@users.noreply.github.com>
2016-12-18 03:01:47 -0600

    patch

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 # Completely rewritten to improve reability
 
 setup(name='darkskypy',
-      version='0.2.01',
+      version='0.3.1',
       description='A different python wrapper to access weather data provided by DarkSky.net',
       url='https://github.com/mattbox/DarkSkyPy',
       author='mattbox',
